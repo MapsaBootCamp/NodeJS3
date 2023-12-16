41230b1111ad512c20182c8c7e63be36b0ca420c
teghfo <teghfo@gmail.com>
2023-12-16 20:00:31 +0330

    W5-J2: crud using prisma

--- hello_prisma/controllers/task.controller.js
+++ hello_prisma/controllers/task.controller.js
@@ -3,8 +3,46 @@ const db = require("../db");
 exports.taskList = async (req, res) => {
   const tasks = await db.task.findMany({
     where: {
-      userId: req.user.username,
+      user: req.user,
     },
   });
   res.json(tasks.map((task) => ({ ...task, id: String(task.id) })));
+};
+
+exports.createTask = async (req, res) => {
+  const { title } = req.body;
+  const task = await db.task.create({
+    data: {
+      title,
+      userId: req.user.username,
+    },
+    select: {
+      id: true,
+      title: true,
+      status: true,
+    },
+  });
+  res.status(201).json({ ...task, id: String(task.id) });
+};
+
+exports.deleteTask = async (req, res) => {
+  const { taskId } = req.params;
+  await db.task.delete({
+    where: {
+      id: taskId,
+      userId: req.user.username,
+    },
+  });
+  return res.status(204).json({ status: "OK" });
+};
+exports.updateTask = async (req, res) => {
+  const { taskId } = req.params;
+  const { title } = req.body;
+  const updatedTask = await db.task.update({
+    where: { id: taskId, userId: req.user.username },
+    data: {
+      title,
+    },
+  });
+  return res.json({ ...updatedTask, id: String(updatedTask.id) });
 };
--- hello_prisma/routes/task.route.js
+++ hello_prisma/routes/task.route.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { taskList } = require("../controllers/task.controller");
+const {
+  taskList,
+  createTask,
+  deleteTask,
+  updateTask,
+} = require("../controllers/task.controller");
 const { userExist } = require("../middleware");
 const router = express.Router();
 
 router.get("/:user/taskList", userExist, taskList);
+router.post("/:user/createTask", userExist, createTask);
+router.put("/:user/:taskId", userExist, updateTask);
+router.delete("/:user/:taskId", userExist, deleteTask);
 
 module.exports = router;
